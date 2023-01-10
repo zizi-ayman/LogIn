45eb07bab8e0a0c4a42921cf356b3143b3a8151c
zizi-ayman <84988483+zizi-ayman@users.noreply.github.com>
2023-01-10 13:06:30 +0100

    Update firebase.php

--- BackEnd/firebase.php
+++ BackEnd/firebase.php
@@ -5,13 +5,11 @@
 //$title=$_POST['title'];
 //$body=$_POST['body'];
 function sendnote($msg){
-    $token='dIXiMX_RQ36Y7pEvk9U2c4:APA91bFq5sOMbrMdzWDOxW6UI1UXU-0w7PZIxEyF3Q0KtpZZbi8KbOe_5GqXIgQCjS5Jz8sh2ROTQpQI6ypbWlEEpkYw5Hl5_eYyOufX-fWchhNQoRhSJIT9YL80z9MJZBxAnEdS6D_c';
+    $token='';
     $title='Notification';
     $body= $msg;
-    define('API_ACCESS_KEY','AAAAm8IjkMQ:APA91bGS1gIV1v9kEZg45I7IYB8WBCZG_JR_5ozUOq42_TMrTMAb42x4rPCJ9vnfHatdqmjJ1S6iHwRGm8D1kErQLycgiN9FPB3aCSwKNmwD59kdYh5Mi-ifUSK9O6eAD5Wodl803UuA');
+    define('API_ACCESS_KEY','');
     $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
-    //$token='ePAMYu6vRsKZDhHTKYfuFI:APA91bGDPhTHkgw8vWvaALWq08GVgr-X2ef8kIrH007lC1b--3SGcbu4x22zzGMmj-SkuKwIGrGYF2_b95Sxv09T8TyDi7GnXHl3mofgDdEUx_TlEyPIfbvkB3qRvupL4v_BRIPXkNCG';
-
     $notification = [
         'title' =>$title,
         'body' => $body,
